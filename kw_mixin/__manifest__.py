98bd131e934ffe0a78bd6fd0ffdfeb300a41daf2
Eng.cabdishakur <abdishakurliban54@gmail.com>
2023-07-12 12:46:37 +0300

    Initial commit

---
+++ kw_mixin/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Mixins',
+    'version': '16.0.1.0.4',
+
+    'author': 'Kitworks Systems',
+    'website': 'https://kitworks.systems/',
+    'license': 'LGPL-3',
+    'category': 'Extra Tools',
+
+    'depends': ['base', ],
+
+    'images': [
+        'static/description/cover.png',
+        'static/description/icon.png',
+    ],
+}
